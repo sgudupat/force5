60b5ff5f7cd7e2dcdafee88bc34cc5363c1a9eb9
sgudupat <sandeep.g@pscglobalsolutions.com>
2016-07-11 12:54:43 +0530

    fixed query

--- src/main/java/com/force/five/app/repository/PaySheetsRepository.java
+++ src/main/java/com/force/five/app/repository/PaySheetsRepository.java
@@ -1,9 +1,8 @@
 package com.force.five.app.repository;
 
-import com.force.five.app.domain.EmployeeSalarySheet;
 import com.force.five.app.domain.PaySheets;
-
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,16 +12,16 @@ import java.util.List;
 public interface PaySheetsRepository extends JpaRepository<PaySheets, Long> {
 
     @Query(nativeQuery = true, value =
-        "select ps.* " +
+        "  select ps.* " +
             "  from client c " +
             "      ,assignments a " +
             "      ,pay_sheets ps " +
             "      ,employee e " +
-            "where c.name = ?1 " +
-            "  and a.client_id = c.id " +
-            "  and a.employee_id = ps.employee_id " +
-            "  and month(ps.start_date) = ?2 " +
-            "  and year(ps.start_date) = ?3 " +
-            "  and ps.employee_id = e.id;")
+            "  where c.name = ?1 " +
+            "    and a.client_id = c.id " +
+            "    and a.employee_id = e.id " +
+            "    and month(a.start_date) = ?2 " +
+            "    and year(a.start_date) = ?3 " +
+            "    and ps.assignments_id = a.id; ")
     List<PaySheets> fetchSalarySheets(String clientName, Integer month, String year);
 }
