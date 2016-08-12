d61c373397a83c511f8314cdf32083ddfda48187
sgudupat <sandeep.g@pscglobalsolutions.com>
2016-08-12 20:31:27 +0530

    modified the UI call

--- src/main/webapp/scripts/app/admin/salary-sheets/salary-sheets.controller.js
+++ src/main/webapp/scripts/app/admin/salary-sheets/salary-sheets.controller.js
@@ -1,30 +1,50 @@
 'use strict';
 
-angular.module('proj2App').controller('SalarySheetsController',SalarySheetsController);
+angular.module('proj2App').controller('SalarySheetsController', SalarySheetsController);
 
-SalarySheetsController.$inject = ['$scope', 'Principal', 'User', 'ParseLinks', 'Client'];
+SalarySheetsController.$inject = ['$scope', 'Client', 'SalarySheets','BillingSheets','InvoiceSheets'];
 
-function SalarySheetsController($scope, Principal, User, ParseLinks, Client) {
+function SalarySheetsController($scope, Client, SalarySheets,BillingSheets,InvoiceSheets) {
     $scope.clients = Client.query();
-    Principal.identity().then(function(account) {
-        $scope.currentAccount = account;
-    });
+
+        $scope.clear = function () {
+            $scope.salarySheets = {
+                clientId: null, month: null, year: null
+            };
+        };
 
     $scope.salaryReport = function () {
        console.log("In save")
        console.dir($scope.salarySheets)
-       SalarySheets.generateSalarySheets($scope.salarySheets, onSaveSuccess, onSaveError);
+       SalarySheets.generateSalarySheets($scope.salarySheets);
     };
 
      $scope.billingReport = function () {
        console.log("In billingReport save")
        console.dir($scope.salarySheets)
-       SalarySheets.generateBillingReport($scope.salarySheets, onSaveSuccess, onSaveError);
+       BillingSheets.generateBillingReport($scope.salarySheets);
     };
 
      $scope.invoiceReport = function () {
        console.log("In invoiceReport save")
        console.dir($scope.salarySheets)
-       SalarySheets.generateInvoiceReport($scope.salarySheets, onSaveSuccess, onSaveError);
+       InvoiceSheets.generateInvoiceReport($scope.salarySheets);
     };
 }
+
+angular.module('proj2App')
+    .factory('SalarySheets',function ($resource, DateUtils) {
+        return $resource('api/salarySheets/generate', {}, {
+            'generateSalarySheets': { method:'PUT' }
+         });
+    })
+    .factory('BillingSheets',function ($resource, DateUtils) {
+        return $resource('api/billingReport/generate', {}, {
+            'generateBillingReport': { method:'PUT' }
+         });
+    })
+     .factory('InvoiceSheets',function ($resource, DateUtils) {
+            return $resource('api/invoiceReport/generate', {}, {
+                'generateInvoiceReport': { method:'PUT' }
+             });
+        });
