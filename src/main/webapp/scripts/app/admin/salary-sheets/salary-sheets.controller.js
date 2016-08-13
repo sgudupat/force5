3c9afe7540d1d62767324ebc858e54cc2389c698
sgudupat <sandeep.g@pscglobalsolutions.com>
2016-08-13 17:30:14 +0530

    Display of message on report

--- src/main/webapp/scripts/app/admin/salary-sheets/salary-sheets.controller.js
+++ src/main/webapp/scripts/app/admin/salary-sheets/salary-sheets.controller.js
@@ -7,28 +7,39 @@ SalarySheetsController.$inject = ['$scope', 'Client', 'SalarySheets','BillingShe
 function SalarySheetsController($scope, Client, SalarySheets,BillingSheets,InvoiceSheets) {
     $scope.clients = Client.query();
 
-        $scope.clear = function () {
-            $scope.salarySheets = {
-                clientId: null, month: null, year: null
-            };
+    $scope.alertMessage = false;
+    $scope.clear = function () {
+        $scope.salarySheets = {
+            clientId: null, month: null, year: null
         };
+    };
+
+    var onSaveSuccess = function (result) {
+       $scope.alertMessage = true;
+       $scope.message = "Report generated successfully";
+       setTimeout(function(){$scope.alertMessage = false;},1000)
+    };
+
+    var onSaveError = function (result) {
+        //do nothing
+    };
 
     $scope.salaryReport = function () {
        console.log("In save")
        console.dir($scope.salarySheets)
-       SalarySheets.generateSalarySheets($scope.salarySheets);
+       SalarySheets.generateSalarySheets($scope.salarySheets, onSaveSuccess, onSaveError);
     };
 
      $scope.billingReport = function () {
        console.log("In billingReport save")
        console.dir($scope.salarySheets)
-       BillingSheets.generateBillingReport($scope.salarySheets);
+       BillingSheets.generateBillingReport($scope.salarySheets,onSaveSuccess,onSaveError);
     };
 
      $scope.invoiceReport = function () {
        console.log("In invoiceReport save")
        console.dir($scope.salarySheets)
-       InvoiceSheets.generateInvoiceReport($scope.salarySheets);
+       InvoiceSheets.generateInvoiceReport($scope.salarySheets,onSaveSuccess,onSaveError);
     };
 }
 
